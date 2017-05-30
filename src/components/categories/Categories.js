79a0f576bfac6ae9cf3da4f93f4d7d14ff3e5ee7
taraojo <tara.ojo@gmail.com>
2017-05-30 01:26:33 +0100

    add selected category styling

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -4,13 +4,27 @@ import CategoryItem from '../categoryItem/CategoryItem'
 import './Categories.css';
 
 class Categories extends Component {
+    constructor(props) {
+        super(props);
+
+        this.selectCategory = this.selectCategory.bind(this);
+        this.state = {
+            selectedCategory: ''
+        };
+    }
+
+    selectCategory(categoryId) {
+        this.setState({selectedCategory: categoryId});
+        this.props.clickHandler(categoryId);
+    }
 
     render() {
         const categoryList = this.props.categoriesData.map(category =>
             <CategoryItem key={category.id}
                           value={category.title}
                           id={category.id}
-                          clickHandler={this.props.clickHandler}/>
+                          clickHandler={this.selectCategory}
+                          selected={this.state.selectedCategory === category.id}/>
         );
 
         return (
--- src/components/categoryItem/CategoryItem.js
+++ src/components/categoryItem/CategoryItem.js
@@ -3,18 +3,16 @@ import PropTypes from 'prop-types';
 import './CategoryItem.css';
 
 class CategoryItem extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            selected: false
-        };
+    categorySelectedClass() {
+        return this.props.selected ? 'category-item__button category-item__button--selected' : 'category-item__button';
     }
 
     render() {
         return (
             <li className="category-item">
-                <button onClick={() => this.props.clickHandler(this.props.id)}>
+                <button onClick={() => this.props.clickHandler(this.props.id)}
+                        className={this.categorySelectedClass()}>
                     <h2>
                         {this.props.value}
                     </h2>
@@ -27,7 +25,8 @@ class CategoryItem extends Component {
 CategoryItem.propTypes = {
     id: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
-    clickHandler: PropTypes.func
+    clickHandler: PropTypes.func,
+    selected: PropTypes.bool
 };
 
 export default CategoryItem;
